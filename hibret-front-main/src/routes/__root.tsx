731d0c586b8ce2268ea4b06ad3034eb020941566
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2024-05-04 19:15:11 +0300

    Update __root.tsx

--- hibret-front-main/src/routes/__root.tsx
+++ hibret-front-main/src/routes/__root.tsx
@@ -19,52 +19,7 @@ function App() {
         </div>
       </div>
 
-      <div className="flex justify-around">
-        <div className="p-2 flex gap-2">
-          <Link to="/" className="[&.active]:font-bold">
-            Home
-          </Link>{" "}
-        </div>
-
-        <div className="p-2">
-          {session ? (
-            <div className="flex gap-4">
-              <button
-                onClick={() => {
-                  deleteSession();
-                }}
-              >
-                Logout
-              </button>
-              <Link to="/profile" className="[&.active]:font-bold">
-                Profile
-              </Link>
-
-              <Link to="/documents" className="[&.active]:font-bold">
-                New Document
-              </Link>
-              <Link to="/my-documents" className="[&.active]:font-bold">
-                My Documents
-              </Link>
-
-              <Link to="/notification" className="[&.active]:font-bold">
-                Notification
-              </Link>
-            </div>
-          ) : (
-            <div className="flex gap-2">
-              <Link to="/login" className="[&.active]:font-bold">
-                Login
-              </Link>
-
-              <Link to="/register" className="[&.active]:font-bold">
-                Register
-              </Link>
-            </div>
-          )}
-        </div>
-      </div>
-
+    
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
