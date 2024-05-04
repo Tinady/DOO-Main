198896b6f13f02360422da0808130e427bb7acfa
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2024-05-04 19:15:18 +0300

    Create adminDashboard.tsx

---
+++ hibret-front-main/src/pages/adminDashboard.tsx
@@ -0,0 +1,19 @@
+import AdminHead from "../components/adminHead";
+import AdminMiddle from "../components/adminMiddle";
+import AdminEnd from "../components/adminEnd";
+
+const adminDashboard = () => {
+  return (
+    <div className="mt-24 ml-80 mr-8">
+         <AdminHead/>
+         <AdminMiddle/>
+         <AdminEnd/>
+
+     
+
+     
+    </div>
+  );
+};
+
+export default adminDashboard;
